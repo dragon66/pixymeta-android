8c1230971ab64fcb9ee03e9d3311b113c2f84e71
dragon66 <yuwen_66@yahoo.com>
2016-11-05 22:23:07 -0400

    Added depth map support for cardboard camera

--- src/pixy/meta/jpeg/JPEGMeta.java
+++ src/pixy/meta/jpeg/JPEGMeta.java
@@ -272,8 +272,13 @@ public class JPEGMeta {
 		if(xmp != null && xmp.hasExtendedXmp()) {
 			Document xmpDocument = xmp.getMergedDocument();
 			String depthMapMime = XMLUtils.getAttribute(xmpDocument, "rdf:Description", "GDepth:Mime");
+			String depthData = "GDepth:Data";
+			if(StringUtils.isNullOrEmpty(depthMapMime)) {
+				depthMapMime = XMLUtils.getAttribute(xmpDocument, "rdf:Description", "GImage:Mime");
+				depthData = "GImage:Data";
+			}
 			if(!StringUtils.isNullOrEmpty(depthMapMime)) {
-				String data = XMLUtils.getAttribute(xmpDocument, "rdf:Description", "GDepth:Data");
+				String data = XMLUtils.getAttribute(xmpDocument, "rdf:Description", depthData);
 				if(!StringUtils.isNullOrEmpty(data)) {
 					String outpath = "";
 					if(pathToDepthMap.endsWith("\\") || pathToDepthMap.endsWith("/"))
