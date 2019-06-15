fcea2555990847571fcfd3c1e34284dbc8a92cdb
Wen Yu <318163045@KKK6943K.maple.fg.rbc.com>
2019-06-15 12:00:35 -0400

    Revert "Handle multiple stripByteCount"
    This reverts commit d49992594f08514cd08a731f5686d5093884bec7.

--- src/pixy/meta/tiff/TIFFMeta.java
+++ src/pixy/meta/tiff/TIFFMeta.java
@@ -355,6 +355,8 @@ public class TIFFMeta {
 				}				
 			} // End of bug fix
 			
+			writeByteCount = counts[0];
+			
 			// We are going to write the image data first
 			rout.seek(writeOffset);
 			
@@ -366,7 +368,6 @@ public class TIFFMeta {
 				rout.write(buf);
 				temp[i] = writeOffset;
 				writeOffset += buf.length;
-				writeByteCount += counts[i];
 			}
 						
 			if(ifd.getField(TiffTag.STRIP_BYTE_COUNTS) != null)
