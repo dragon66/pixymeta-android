d0229541d966732ad7617969d02ffac3e8050a2e
tenzap <fabstz-it@yahoo.fr>
2021-03-29 23:40:23 +0200

    update license header to EPL-2.0 with secondary license

--- src/pixy/meta/MetadataEntry.java
+++ src/pixy/meta/MetadataEntry.java
@@ -1,13 +1,17 @@
-/**
- * COPYRIGHT (C) 2014-2017 WEN YU (YUWEN_66@YAHOO.COM) ALL RIGHTS RESERVED.
+/*
+ * Copyright (c) 2014-2021 by Wen Yu
  *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Any modifications to this file must keep this entire header intact.
- * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * or any later version.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ *
  * Change History - most recent changes go on top of previous changes
  *
  * MetadataEntry.java
--- src/pixy/meta/tiff/TIFFMeta.java
+++ src/pixy/meta/tiff/TIFFMeta.java
@@ -1,11 +1,16 @@
-/**
- * Copyright (C) 2014-2019 by Wen Yu.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Any modifications to this file must keep this entire header intact.
+/*
+ * Copyright (c) 2014-2021 by Wen Yu
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * or any later version.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
  *
  * Change History - most recent changes go on top of previous changes
  *
